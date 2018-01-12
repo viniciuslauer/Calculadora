5bdb0088b79b624a9bc18baa4bb935a04840dfb9
vinicius lauer <viniciuslauer@hotmail.com>
2018-01-12 12:03:49 -0200

    Excluida a classe Somar, foi substituída pela classe Calculadora (que contém as 4 operações matemáticas).

---
+++ Calculadora/Classes/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Classes
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// Parâmetro 1 + parâmetro 2
+        /// </summary>
+        /// <param name="numeroUm">Primeiro número</param>
+        /// <param name="numeroDois">Segundo número</param>
+        /// <returns></returns>
+        public double Somar(double numeroUm, double numeroDois)
+        {
+            return numeroUm + numeroDois;
+        }
+
+        /// <summary>
+        /// Parâmetro 1 - parâmetro 2
+        /// </summary>
+        /// <param name="numeroUm">Primeiro número</param>
+        /// <param name="numeroDois">Segundo número</param>
+        /// <returns></returns>
+        public double Subtrair(double numeroUm, double numeroDois)
+        {
+            return numeroUm - numeroDois;
+        }
+
+        /// <summary>
+        /// Parâmetro 1 * parâmetro 2.
+        /// </summary>
+        /// <param name="numeroUm">The numero um.</param>
+        /// <param name="numeroDois">The numero dois.</param>
+        /// <returns></returns>
+        public double Multiplicar(double numeroUm, double numeroDois)
+        {
+            return numeroUm * numeroDois;
+        }
+
+        /// <summary>
+        /// Parâmetro 1 / parâmetro 2
+        /// </summary>
+        /// <param name="numeroUm">The numero um.</param>
+        /// <param name="numeroDois">The numero dois.</param>
+        /// <returns></returns>
+        public double Dividir(double numeroUm, double numeroDois)
+        {
+            return numeroUm / numeroDois;
+        }
+    }
+}
